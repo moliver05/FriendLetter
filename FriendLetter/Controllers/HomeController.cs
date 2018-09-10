4faa6d26c3c02cef1c7191be4448fb92633d1835
Mark Mangahas <mo.mangahas@yahoo.com>
2018-09-10 11:14:09 -0700

    added codes pt.2

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -1,13 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
+using FriendLetter.Models;
 
 namespace FriendLetter.Controllers
 {
     public class HomeController : Controller
+
+  {
+    [Route("/")]
+    public ActionResult Hello()
     {
-        [Route("/")]
-        public ActionResult Journal()
-        {
-          return View();
-        }
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.SetRecipient("Lina");
+      myLetterVariable.SetSender("John");
+      return View(myLetterVariable);
+    }
   }
 }
